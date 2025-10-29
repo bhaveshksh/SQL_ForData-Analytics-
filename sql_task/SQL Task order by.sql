61fce8237adc00a6e00f9247bb6af7527e0cc403
bhaveshksh <bhaveshkshirsagar50@gmail.com>
2025-10-29 17:51:41 +0530

    Initial commit

--- sql_task/SQL Task order by.sql
+++ sql_task/SQL Task order by.sql
@@ -1,2 +1,2 @@
 -- # task how to do asc product_id and desc qty
-select ecom_data.product_id,qty from ecom_data order by ecom_data.product_id asc,qty desc
+select sales.product_id,qty from sales order by sales.product_id asc,qty desc
