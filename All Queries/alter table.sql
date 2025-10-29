24594b28c69a9e5ca09ed85f028c0f36f58c8228
bhaveshksh <bhaveshkshirsagar50@gmail.com>
2025-10-29 17:22:22 +0530

    Initial commit

--- All Queries/alter table.sql
+++ All Queries/alter table.sql
@@ -14,4 +14,38 @@ select id, name,dept[1] from collegee
 alter table collegee 
 drop column dept;
 select * from collegee
-alter table collegee add column col2 varchar not null default '34'
+alter table collegee add column col2 varchar not null default '34';
+
+update collegee set grade = 'FA' where id = 4
+update collegee set grade = 'F' where id = 11
+
+alter table collegee
+alter column grade type varchar;
+select * from collegee
+
+alter table collegee
+alter column total_staff type varchar;
+select * from collegee
+
+select * from collegee where total_staff > 7
+
+alter table collegee
+alter column total_staff type int
+using total_staff::integer; -- cascading this is used for to change data type for limit time
+
+select * from collegee
+
+select collegee.total_student::varchar from collegee
+
+update collegee set id = 11 where id = 4
+
+alter table collegee 
+alter column id type not null
+
+alter table collegee add constraint total_staff_limit check (total_staff >= 20 and total_staff <= 40)
+
+select * from collegee where total_staff >= 20 and total_staff <= 40
+
+update collegee set total_staff = 36 where total_staff not between 20 and 40
+
+select * from collegee where total_staff = 46
---
+++ sql_task/SQL Task order by.sql
@@ -0,0 +1,2 @@
+-- # task how to do asc product_id and desc qty
+select ecom_data.product_id,qty from ecom_data order by ecom_data.product_id asc,qty desc
