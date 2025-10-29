5ba4803cb9995f2be620ad383ddf0c663a470d94
bhaveshksh <bhaveshkshirsagar50@gmail.com>
2025-10-29 17:57:04 +0530

    Uploading file

---
+++ All Queries/coping data from existing table.sql
@@ -0,0 +1,24 @@
+select * from college_data
+select * from college_data
+order by id
+
+select * from college_data
+order by address
+
+update college_data set address = '123' where id = 4
+
+select * from college_data
+
+update college_data set address = '000' where id = 6
+
+update college_data set address = '124' where id = 5
+
+update college_data set address = null where id = 7
+
+update college_data set address = '' where id = 2
+
+select * from college_data order by address
+
+select * from college_data order by address desc
+
+
